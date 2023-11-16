d860ca433e9190af5ac9394331c423379a4b874c
Mart <mcderoos@hotmail.com>
2023-11-16 16:54:49 +0100

    added configuration option for disabling simultaneous testing added simultaneous testing and smart bail to test runner capabilities

--- packages/api/src/test-runner/test-runner-capabilities.ts
+++ packages/api/src/test-runner/test-runner-capabilities.ts
@@ -8,4 +8,14 @@ export interface TestRunnerCapabilities {
    * When true, the test runner should reload the test environment when `reloadEnvironment` is present in the run options.
    */
   reloadEnvironment: boolean;
+  /**
+   * When true, the test runner is capable of performing simultaneous mutation testing. False otherwise.
+   * When false, the planner will never provide a test plan that tests more than 1 mutant at the same time.
+   */
+  simultaneousTesting?: boolean;
+  /**
+   * When true, the test runner is capable of bailing only the subset of tests that are associated with the killed simultaneous
+   * mutant. False otherwise. The test planner might change the schedule in the case that smart bail is not available.
+   */
+  smartBail?: boolean;
 }
